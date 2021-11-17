958e285b9316c58970e44de4d8caa24b7bd25c00
Milos Vasic <milos85vasic@gmail.com>
2021-11-17 15:10:28 +0300

    FFM-1683 - Publishing gem.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,6 +6,16 @@ dir_path=$(dirname "$full_path")
 . "$dir_path/sdk_specs.sh"
 
 # shellcheck disable=SC2154
-cd "$dir_path/.." && gem build "$ff_ruby_sdk.gemspec" && \
-  echo "Please enter your 'sudo' password if asked" && sudo gem install "$ff_ruby_sdk" && \
-  gem push "$ff_ruby_sdk-$ff_ruby_sdk_version.gem"
+if cd "$dir_path/.." && gem build "$ff_ruby_sdk.gemspec"; then
+
+  if echo "Please enter your 'sudo' password if asked" && sudo gem install "$ff_ruby_sdk"; then
+
+    gem push "$ff_ruby_sdk-$ff_ruby_sdk_version.gem"
+  else
+
+    exit 1
+  fi
+else
+
+  exit 1
+fi
