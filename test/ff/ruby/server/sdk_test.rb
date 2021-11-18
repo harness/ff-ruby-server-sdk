69ed584998c097dd271bce76bec8fb19b69df4a9
Milos Vasic <milos85vasic@gmail.com>
2021-11-18 14:02:11 +0300

    FFM-1683 - Instantiation unit test.

--- test/ff/ruby/server/sdk_test.rb
+++ test/ff/ruby/server/sdk_test.rb
@@ -8,6 +8,20 @@ require "minitest/autorun"
 class Ff::Ruby::Server::SdkTest < Minitest::Test
 
   def test_version_number
+
     refute_nil ::Ff::Ruby::Server::Sdk::VERSION
+  end
+
+  def test_instantiation
+
+    instance = CfClient.instance
+    (0..5).each {
+
+      compare_equal = CfClient.instance
+      compare_not_equal = CfClient.new
+
+      assert_equal(instance.to_s, compare_equal.to_s)
+      assert(instance.to_s != compare_not_equal.to_s)
+    }
   end
 end
