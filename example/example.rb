97bd3861756fc7eaa52c43669189d4d9e71b12c4
Milos Vasic <milos85vasic@gmail.com>
2021-11-18 13:48:23 +0300

    FFM-1683 - Singleton: obtain the instance.

--- example/example.rb
+++ example/example.rb
@@ -1,4 +1,9 @@
 require_relative '../lib/ff/ruby/server/sdk'
 
-client = CfClient.new
+client = CfClient.instance
+client2 = CfClient.new
+client3 = CfClient.instance
+
 client.hello
+client2.hello
+client3.hello
--- lib/ff/ruby/server/sdk/cf_client.rb
+++ lib/ff/ruby/server/sdk/cf_client.rb
@@ -1,6 +1,18 @@
 class CfClient
 
+  # Static:
+  class << self
+
+    @@instance = CfClient.new
+
+    def instance
+
+      @@instance
+    end
+  end # Static - End
+
   def hello
-    puts "Hello from the client"
+
+    puts "Hello from the client: " + self.to_s
   end
 end
