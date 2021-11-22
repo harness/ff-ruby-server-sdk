ff2900d7eb0c61eab822332d1b14fce97451a1c9
Milos Vasic <milos85vasic@gmail.com>
2021-11-22 18:22:12 +0300

    FFM-1683 - Instantiation test.

--- test/ff/ruby/server/sdk/client_instantiation_test.rb
+++ test/ff/ruby/server/sdk/client_instantiation_test.rb
@@ -5,7 +5,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ff/ruby/server/sdk"
 require "minitest/autorun"
 
-class Ff::Ruby::Server::SdkClientInstantiationTest < Minitest::Test
+class Ff::Ruby::Server::ClientInstantiationTest < Minitest::Test
 
   def test_version_number
 
