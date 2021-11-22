a3f95bc7194706b55dca6facafc98dab1b433982
Milos Vasic <milos85vasic@gmail.com>
2021-11-22 18:49:25 +0300

    FFM-1683 - The first config test.

--- test/ff/ruby/server/sdk/sdk_test.rb
+++ test/ff/ruby/server/sdk/sdk_test.rb
@@ -5,14 +5,14 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ff/ruby/server/sdk"
 require "minitest/autorun"
 
-class Ff::Ruby::Server::ClientInstantiationTest < Minitest::Test
+class Ff::Ruby::Server::SdkTest < Minitest::Test
 
   def test_version_number
 
     refute_nil ::Ff::Ruby::Server::Sdk::VERSION
   end
 
-  def test_singleton_instantiation
+  def test_client_singleton_inst
 
     instance = CfClient.instance
     (0..5).each {
@@ -22,7 +22,7 @@ class Ff::Ruby::Server::ClientInstantiationTest < Minitest::Test
     }
   end
 
-  def test_constructor_instantiation
+  def test_client_constructor_inst
 
     test_string = "test"
     instance_with = Client.new(test_string)
@@ -31,5 +31,15 @@ class Ff::Ruby::Server::ClientInstantiationTest < Minitest::Test
     assert(instance_with != nil)
     assert(instance_with_no_config != nil)
     assert(instance_with != instance_with_no_config)
+  end
+
+  def test_config_constructor_inst
+
+    config = Config.new
+    config_not_equal = Config.new
+
+    assert(config != nil)
+    assert(config_not_equal != nil)
+    assert(config != config_not_equal)
   end
 end
