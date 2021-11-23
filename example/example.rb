5b544c4247b28d418645c43f7c5fa13f2b209fe6
Milos Vasic <milos85vasic@gmail.com>
2021-11-23 19:37:51 +0300

    FFM-1683 - Example updated.

--- example/example.rb
+++ example/example.rb
@@ -1,15 +1,35 @@
-require_relative '../lib/ff/ruby/server/sdk/api/client'
 require_relative '../lib/ff/ruby/server/sdk/api/cf_client'
 require_relative '../lib/ff/ruby/server/sdk/api/inner_client'
 require_relative '../lib/ff/ruby/server/sdk/api/config'
+require_relative '../lib/ff/ruby/server/sdk/api/config_builder'
+
+bool = false
+number = 100
+string = "test"
+
+config = ConfigBuilder.new
+                      .event_url(string)
+                      .config_url(string)
+                      .stream_enabled(bool)
+                      .poll_interval_in_seconds(number)
+                      .analytics_enabled(bool)
+                      .frequency(number)
+                      .buffer_size(number)
+                      .all_attributes_private(!bool)
+                      .private_attributes(Set[string])
+                      .connection_timeout(number)
+                      .read_timeout(number)
+                      .write_timeout(number)
+                      .debug(!bool)
+                      .metrics_service_acceptable_duration(number)
+                      .build
+
+puts config.describe
 
 client = CfClient.instance
-client2 = Client.new("aa", "bb")
+client2 = CfClient.new(string, config)
 client3 = CfClient.instance
 
 client.hello
 client2.hello
 client3.hello
-
-configuration = Config.new
-puts configuration.describe
