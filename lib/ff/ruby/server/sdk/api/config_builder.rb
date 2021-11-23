ed82cc4737f150475a669020d557efe96d2de4ec
Milos Vasic <milos85vasic@gmail.com>
2021-11-23 19:12:57 +0300

    FFM-1683 - Builder completed.

--- lib/ff/ruby/server/sdk/api/config_builder.rb
+++ lib/ff/ruby/server/sdk/api/config_builder.rb
@@ -22,4 +22,87 @@ class ConfigBuilder
     self
   end
 
+  def stream_enabled(stream_enabled)
+
+    @config.stream_enabled = stream_enabled
+    self
+  end
+
+  def poll_interval_in_seconds(poll_interval_in_seconds)
+
+    @config.poll_interval_in_seconds = poll_interval_in_seconds
+    self
+  end
+
+  def analytics_enabled(analytics_enabled)
+
+    @config.analytics_enabled = analytics_enabled
+    self
+  end
+
+  def frequency(frequency)
+
+    @config.frequency = frequency
+    self
+  end
+
+  def buffer_size(buffer_size)
+
+    @config.buffer_size = buffer_size
+    self
+  end
+
+  def all_attributes_private(all_attributes_private)
+
+    @config.all_attributes_private = all_attributes_private
+    self
+  end
+
+  def private_attributes(private_attributes)
+
+    @config.private_attributes = private_attributes
+    self
+  end
+
+  def connection_timeout(connection_timeout)
+
+    @config.connection_timeout = connection_timeout
+    self
+  end
+
+  def read_timeout(read_timeout)
+
+    @config.read_timeout = read_timeout
+    self
+  end
+
+  def write_timeout(write_timeout)
+
+    @config.write_timeout = write_timeout
+    self
+  end
+
+  def debug(debug)
+
+    @config.debug = debug
+    self
+  end
+
+  def metrics_service_acceptable_duration(metrics_service_acceptable_duration)
+
+    @config.metrics_service_acceptable_duration = metrics_service_acceptable_duration
+    self
+  end
+
+  def cache(cache)
+
+    @config.cache = cache
+    self
+  end
+
+  def store(store)
+
+    @config.store = store
+    self
+  end
 end
