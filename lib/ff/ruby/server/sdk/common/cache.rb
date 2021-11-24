318c8f6cb72490e89f4cc029ea8cee795a63c09f
Milos Vasic <milos85vasic@gmail.com>
2021-11-24 12:38:17 +0300

    FFM-1684 - Cache abstraction.

---
+++ lib/ff/ruby/server/sdk/common/cache.rb
@@ -0,0 +1,27 @@
+class Cache
+
+  def initialize
+
+    @tbe = "to be implemented"
+  end
+
+  def set(key, value)
+
+    raise @tbe
+  end
+
+  def get(key)
+
+    raise @tbe
+  end
+
+  def delete(key)
+
+    raise @tbe
+  end
+
+  def keys
+
+    raise @tbe
+  end
+end
