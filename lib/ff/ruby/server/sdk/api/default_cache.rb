3e2f5eb2518c1b441a52083ae7ab2e8f00745418
Milos Vasic <milos85vasic@gmail.com>
2021-11-24 13:25:06 +0300

    FFM-1684 - Default cache - Initialized.

--- lib/ff/ruby/server/sdk/api/default_cache.rb
+++ lib/ff/ruby/server/sdk/api/default_cache.rb
@@ -5,24 +5,22 @@ class DefaultCache < Cache
   def initialize
     super
 
+    @capacity = 10 * 1000
+
     @in_memory = CacheBuilder.with(Cache)
-                             .set_expiry('3s')
-                             .set_max(500)
-                             .set_refresh(lambda { |key| key + 100 })
-                             .set_post_get(lambda { |*key| puts "Retrieved #{key}!" })
+                             .set_max(@capacity)
+                             .set_post_get(lambda { |*key| puts "Retrieved #{key}" })
                              .build
 
     @filesystem = CacheBuilder.with(FileCache)
-                              .set_store("cache")
-                              .set_expiry('3s')
-                              .set_max(500)
-                              .set_refresh(lambda { |key| key + 100 })
-                              .set_post_get(lambda { |*key| puts "Retrieved #{key}!" })
+                              .set_store("sdk/cache")
+                              .set_max(@capacity)
+                              .set_post_get(lambda { |*key| puts "Retrieved #{key}" })
                               .build
   end
 
   def verify
 
-    @in_memory != nil && @filesystem != nil
+    @in_memory != nil && @filesystem != nil && @capacity > 0
   end
 end
