1a49542dae458cce2ff0f95a63a9439ce3799636
Milos Vasic <milos85vasic@gmail.com>
2021-12-06 14:32:07 +0100

    FFM-1684 - Storage.

--- lib/ff/ruby/server/sdk/common/storage.rb
+++ lib/ff/ruby/server/sdk/common/storage.rb
@@ -1,6 +1,28 @@
-class Storage < Cache
+require_relative "closeable"
 
-  def clone
+class Storage < Closeable
+
+  def initialize
+
+    @tbe = "To be implemented"
+  end
+
+  def set(key, value)
+
+    raise @tbe
+  end
+
+  def get(key)
+
+    raise @tbe
+  end
+
+  def delete(key)
+
+    raise @tbe
+  end
+
+  def keys
 
     raise @tbe
   end
