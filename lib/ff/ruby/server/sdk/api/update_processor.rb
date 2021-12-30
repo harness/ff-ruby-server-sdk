d8c261721d211388f2439c4d983f2babc6c77cf7
Milos Vasic <milos85vasic@gmail.com>
2021-12-30 13:29:48 +0300

    FFM-1687 - Processor methods.

--- lib/ff/ruby/server/sdk/api/update_processor.rb
+++ lib/ff/ruby/server/sdk/api/update_processor.rb
@@ -2,5 +2,56 @@ require_relative "../common/closeable"
 
 class UpdateProcessor < Closeable
 
+  def initialize(
 
+    connector,
+    repository,
+    callback
+  )
+
+    @connector = connector
+    @repository = repository
+    @updater = callback
+  end
+
+  def start
+
+  end
+
+  def stop
+
+  end
+
+  def update(message)
+
+  end
+
+  protected
+
+  def process_flag(message)
+
+    nil
+  end
+
+  def process_segment(message)
+
+    nil
+  end
+
+  public def close
+
+    puts "Closing UpdateProcessor"
+
+    stop
+
+    if @stream != nil
+
+      unless @stream.kind_of?(Closeable)
+
+        raise "The 'stream' must be of '" + Closeable.to_s + "' data type"
+      end
+
+      @stream.close
+    end
+  end
 end
