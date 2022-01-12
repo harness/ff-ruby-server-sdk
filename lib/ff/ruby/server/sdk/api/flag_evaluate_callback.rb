0e6fd7a25c13ab86bf482fd3aed96bfe949662f9
Milos Vasic <milos85vasic@gmail.com>
2022-01-12 13:33:55 +0300

    FFM-2082 - The evaluation callback.

---
+++ lib/ff/ruby/server/sdk/api/flag_evaluate_callback.rb
@@ -0,0 +1,13 @@
+class FlagEvaluateCallback
+
+  def initialize
+    super
+
+    @tbi = "To be implemented"
+  end
+
+  def process_evaluation(feature_config, target, variation)
+
+    raise @tbi
+  end
+end
