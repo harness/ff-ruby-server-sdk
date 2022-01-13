3053b778175d09fa4fede84eb8f21595c02d8f47
Milos Vasic <milos85vasic@gmail.com>
2022-01-13 12:37:00 +0300

    FFM-2082 - The evaluator methods.

--- lib/ff/ruby/server/sdk/api/evaluator.rb
+++ lib/ff/ruby/server/sdk/api/evaluator.rb
@@ -1,3 +1,5 @@
+require "json"
+
 require_relative "evaluation"
 require_relative "../common/repository"
 
@@ -10,26 +12,128 @@ class Evaluator < Evaluation
       raise "The 'repository' parameter must be of '" + Repository.to_s + "' data type"
     end
 
+    @one_hundred = 100
+
     @repository = repository
   end
 
   def bool_variation(identifier, target, default_value, callback)
 
+    val variation = evaluate(identifier, target, "boolean", callback)
 
+    if variation != nil
+
+      return variation.value == "true"
+    end
+
+    default_value
   end
 
   def string_variation(identifier, target, default_value, callback)
 
+    val variation = evaluate(identifier, target, "string", callback)
 
+    if variation != nil
+
+      return variation.value
+    end
+
+    default_value
   end
 
   def number_variation(identifier, target, default_value, callback)
 
+    val variation = evaluate(identifier, target, "int", callback)
 
+    if variation != nil
+
+      return variation.value.to_i
+    end
+
+    default_value
   end
 
   def json_variation(identifier, target, default_value, callback)
 
+    val variation = evaluate(identifier, target, "json", callback)
+
+    if variation != nil
+
+      return JSON.parse(variation.value)
+    end
+
+    default_value
+  end
+
+  def evaluate(identifier, target, expected, callback)
+
+    unless callback.kind_of?(FlagEvaluateCallback)
+
+      raise "The 'callback' parameter must be of '" + FlagEvaluateCallback.to_s + "' data type"
+    end
+
+
+  end
+
+  protected
+
+  def get_attr_value(target, attribute)
+
+
+  end
+
+  def find_variation(variations, identifier)
+
+
+  end
+
+  def get_normalized_number(property, bucket_by)
+
+  end
+
+  def is_enabled(target, bucket_by, percentage)
+
+  end
+
+  def evaluate_distribution(distribution, target)
+
+  end
+
+  def evaluate_clauses(clauses, target)
+
+  end
+
+  def evaluate_clause(clause, target)
+
+  end
+
+  def is_target_included_or_excluded_in_segment(segment_list, target)
+
+  end
+
+  def evaluate_rules(serving_rules, target)
+
+  end
+
+  def evaluate_rule(serving_rule, target)
+
+  end
+
+  def evaluate_variation_map(variation_maps, target)
+
+  end
+
+  def evaluate_flag(feature_config, target)
+
+  end
+
+  def check_requisite(parent_feature_config, target)
+
+  end
+
+  private
+
+  def is_target_in_list(target, list_of_target)
 
   end
 end
