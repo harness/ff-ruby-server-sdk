339bf198ed686bcd8a0b9cf01cd7064a6398f80f
Milos Vasic <milos85vasic@gmail.com>
2022-01-18 12:16:13 +0300

    FFM-2083 - Evaluator integration test - Log the beginning and the end.

--- test/ff/ruby/server/sdk/evaluator_integration_test.rb
+++ test/ff/ruby/server/sdk/evaluator_integration_test.rb
@@ -2,6 +2,11 @@ class EvaluatorIntegrationTest
 
   def execute
 
+    puts "The evaluator integration test: START"
+
+    # TODO: Implementation of the test
+
+    puts "The evaluator integration test: END"
     true
   end
 
