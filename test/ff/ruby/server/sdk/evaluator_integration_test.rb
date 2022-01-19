4824664138ebdb73f3b0d24ad9afdae6012f1775
Milos Vasic <milos85vasic@gmail.com>
2022-01-19 16:52:41 +0300

    FFM-2083 - Conversion from hash into the OpenAPI objects.

--- test/ff/ruby/server/sdk/evaluator_integration_test.rb
+++ test/ff/ruby/server/sdk/evaluator_integration_test.rb
@@ -72,6 +72,127 @@ class EvaluatorIntegrationTest < Minitest::Test
 
         model["flag"]["feature"] = feature
 
+        flag_hash = model["flag"]
+
+        refute_nil flag_hash
+
+        flag_hash["default_serve"] = OpenapiClient::Serve.new(flag_hash["defaultServe"])
+        flag_hash.delete("defaultServe")
+
+        dist = flag_hash["default_serve"].distribution
+
+        if dist != nil
+
+          flag_hash["default_serve"].distribution = OpenapiClient::Distribution.new(dist)
+        end
+
+        flag_hash["off_variation"] = flag_hash["offVariation"]
+        flag_hash.delete("offVariation")
+
+        flag_hash["variation_to_target_map"] = flag_hash["variationToTargetMap"]
+        flag_hash.delete("variationToTargetMap")
+
+        flag_hash["state"] = OpenapiClient::FeatureState.build_from_hash(flag_hash["state"])
+
+        variations = []
+
+        flag_hash["variations"].each do |v|
+
+          variation = OpenapiClient::Variation.new(v)
+
+          refute_nil variation
+
+          variations.push(variation)
+
+          assert !variations.empty?
+        end
+
+        flag_hash["variations"] = variations
+
+        rules = []
+
+        flag_hash["rules"].each do |v|
+
+          refute_nil v
+
+          v["rule_id"] = v["ruleId"]
+          v.delete("ruleId")
+
+          clauses = []
+
+          if v["clauses"] != nil
+
+            v["clauses"].each do |c|
+
+              clause = OpenapiClient::Clause.new(c)
+
+              refute_nil clause
+
+              clauses.push(clause)
+
+              assert !clauses.empty?
+            end
+          end
+
+          v["clauses"] = clauses
+
+          v["serve"] = OpenapiClient::Serve.new(v["serve"])
+
+          rule = OpenapiClient::ServingRule.new(v)
+
+          refute_nil rule
+
+          rules.push(rule)
+
+          assert !rules.empty?
+        end
+
+        flag_hash["rules"] = rules
+
+        prerequisites = []
+
+        flag_hash["prerequisites"].each do |v|
+
+          prerequisite = OpenapiClient::Prerequisite.new(v)
+
+          refute_nil prerequisite
+
+          prerequisites.push(prerequisite)
+
+          assert !prerequisites.empty?
+        end
+
+        flag_hash["prerequisites"] = prerequisites
+
+        variation_to_target_map = []
+
+        if flag_hash["variation_to_target_map"] != nil
+
+          flag_hash["variation_to_target_map"].each do |v|
+
+            refute_nil v
+
+            v["target_segments"] = v["targetSegments"]
+            v.delete("targetSegments")
+
+            map = OpenapiClient::VariationMap.new(v)
+
+            refute_nil map
+
+            variation_to_target_map.push(map)
+
+            assert !variation_to_target_map.empty?
+          end
+        end
+
+        flag_hash["variation_to_target_map"] = variation_to_target_map
+
+        flag = OpenapiClient::FeatureConfig.new(flag_hash)
+
+        refute_nil flag
+
+        model["flag"] = flag
+
         @test_data.push(model)
 
         assert !@test_data.empty?
@@ -82,5 +203,4 @@ class EvaluatorIntegrationTest < Minitest::Test
       end
     end
   end
-
 end
