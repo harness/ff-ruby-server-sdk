1fb9a2e9b9ceb0e965961ebeccaa998767bd6982
Milos Vasic <milos85vasic@gmail.com>
2022-02-02 15:05:05 +0300

    FFM-2208 - Example script updated.

--- example/example.rb
+++ example/example.rb
@@ -6,61 +6,95 @@ require_relative '../lib/ff/ruby/server/sdk/api/config'
 require_relative '../lib/ff/ruby/server/sdk/api/cf_client'
 require_relative '../lib/ff/ruby/server/sdk/api/config_builder'
 
-client = CfClient.instance
+flag_b = "flag1"
+flag_n = "flag2"
+flag_s = "flag3"
+flag_j = "flag4"
 
-# Non-Freemium:
-# key = "e22567cb-ccbd-4460-aa8a-751c24c7efda"
-
-# Freemium:
-# key = "76c48e81-674a-41a0-beee-838f44ee45b5"
-
-# UAT:
-key = "8b00e7d0-3415-4c1e-a75b-f2b449e39ccc"
+clients = {}
+targets = {}
 
 logger = Logger.new(STDOUT)
-# logger = Logger.new("example.log", "daily")
 
-config = ConfigBuilder.new
-                      .config_url("https://config.feature-flags.uat.harness.io/api/1.0")
-                      .event_url("https://event.feature-flags.uat.harness.io/api/1.0")
-                      .logger(logger)
-                      .build
+executor = Concurrent::FixedThreadPool.new(100)
 
-client.init(
+keys = {
 
-  key,
-  config
-)
+  "Freemium" => "1f3339b4-e004-457a-91f7-9b5ce173eaaf",
+  "Non-Freemium" => "a30cf6aa-67f2-4545-8ac7-f86709f4f3a0"
+}
 
-config.logger.debug "We will wait for the initialization"
+keys.each do |name, key|
 
-client.wait_for_initialization
+  targets[name] = Target.new("ruby_target_" + name)
 
-config.logger.debug "Initialization is complete"
+  config = ConfigBuilder.new
+                        .logger(logger)
+                        .build
 
-target = Target.new("ruby_target_" + SecureRandom.uuid.to_s)
+  client = CfClient.new(key, config)
 
-bool_flag = "flag1"
-number_flag = "flag2"
-string_flag = "flag3"
-json_flag = "flag4"
+  # .config_url("https://config.feature-flags.uat.harness.io/api/1.0")
+  # .event_url("https://event.feature-flags.uat.harness.io/api/1.0")
+
+  client.init
+
+  config.logger.debug "We will wait for the initialization"
+
+  client.wait_for_initialization
+
+  config.logger.debug "Initialization is complete"
+
+  clients[name] = client
+end
 
 iterations = 10
 
-(0..iterations).each do
+counted = 0
+count_to = keys.size * iterations
 
-  bool_result = client.bool_variation(bool_flag, target, false)
-  number_result = client.number_variation(number_flag, target, -1)
-  string_result = client.string_variation(string_flag, target, "unavailable !!!")
-  json_result = client.json_variation(json_flag, target, JSON.parse("{}"))
+logger.debug "To count: " + count_to.to_s
 
-  config.logger.debug "'" + bool_flag.to_s + "' has the value of: " + bool_result.to_s
-  config.logger.debug "'" + number_flag.to_s + "' has the value of: " + number_result.to_s
-  config.logger.debug "'" + string_flag.to_s + "' has the value of: " + string_result.to_s
-  config.logger.debug "'" + json_flag.to_s + "' has the value of: " + json_result.to_s
+keys.each do |name, key|
 
-  sleep 10
+  client = clients[name]
+  target = targets[name]
+
+  executor.post do
+
+    (1..iterations).each do |iteration|
+
+      logger.debug name + " :: iteration no: " + iteration.to_s
+
+      bool_result = client.bool_variation(flag_b, target, false)
+      number_result = client.number_variation(flag_n, target, -1)
+      string_result = client.string_variation(flag_s, target, "unavailable !!!")
+      json_result = client.json_variation(flag_j, target, JSON.parse("{}"))
+
+      logger.debug name + " :: '" + flag_b.to_s + "' has the value of: " + bool_result.to_s
+      logger.debug name + " :: '" + flag_n.to_s + "' has the value of: " + number_result.to_s
+      logger.debug name + " :: '" + flag_s.to_s + "' has the value of: " + string_result.to_s
+      logger.debug name + " :: '" + flag_j.to_s + "' has the value of: " + json_result.to_s
+      logger.debug "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+
+      counted = counted + 1
+
+      logger.debug "Counted: " + counted.to_s
+
+      sleep 10
+    end
+  end
 end
 
-client.close
+while counted != count_to
+
+  sleep(1)
+end
+
+clients.each do |name, client|
+
+  logger.debug name + " :: closing"
+
+  client.close
+end
 
