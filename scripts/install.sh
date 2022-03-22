bc6118ebb5f169d7a895b9709a00c6edeeac2163
Milos Vasic <milos85vasic@gmail.com>
2022-03-22 18:54:50 +0300

    FFM-2541 - Shell scripts updated.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,9 +8,9 @@ dir_path=$(dirname "$full_path")
 # shellcheck disable=SC2154
 if cd "$dir_path/.." && gem build "$ff_ruby_sdk.gemspec" && gem install "$ff_ruby_sdk"; then
 
-  echo "SUCCESS: Installed"
+  echo "$ff_ruby_sdk installed with success"
 else
 
-  echo "ERROR: FF Gem not installed"
+  echo "ERROR: $ff_ruby_sdk not installed"
   exit 1
 fi
