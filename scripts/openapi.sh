164592cc01ec977e570c24dd9d1a64745834276d
Milos Vasic <milos85vasic@gmail.com>
2022-03-23 12:48:44 +0300

    FFM-2541 - Updating OpenApi script.

--- scripts/openapi.sh
+++ scripts/openapi.sh
@@ -3,11 +3,23 @@
 full_path=$(realpath "$0")
 dir_path=$(dirname "$full_path")
 
+if [ -z "$1" ]
+  then
+
+    installation_path="$dir_path/.."
+    echo "No installation has been provided, using the default path: $installation_path"
+else
+
+    installation_path="$1"
+    echo "Installation path provided: $installation_path"
+fi
+
+
 . "$dir_path/sdk_specs.sh"
 
 if which openapi-generator-cli; then
 
-  generated_path="$dir_path/../lib/ff/ruby/server/generated"
+  generated_path="$installation_path/lib/ff/ruby/server/generated"
 
   if test -e "$generated_path"; then
 
@@ -32,7 +44,7 @@ if which openapi-generator-cli; then
   fi
 
   cd "$dir_path/.." &&
-    openapi-generator-cli generate -i api.yaml -g ruby -o "$generated_path" &&
+    openapi-generator-cli generate -i api.yaml -g ruby -o "$installation_path" &&
     cd "$generated_path" && gem build openapi_client.gemspec && cd .. &&
     gem install "$generated_path/openapi_client-1.0.0.gem" &&
     echo "Generated API has been installed with success: $generated_path"
